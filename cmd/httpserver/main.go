d4f737cb29564463e52db06660044d8e070c53c9
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-10-27 20:42:30 +0100

    Utilizing new feature

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -4,7 +4,6 @@ import (
 	"httpprotocol/internal/request"
 	"httpprotocol/internal/response"
 	"httpprotocol/internal/server"
-	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -13,30 +12,80 @@ import (
 
 const port = 42069
 
+func respond400() []byte {
+	return []byte(`
+	<html>
+	  <head>
+		<title>400 Bad Request</title>
+	  </head>
+	  <body>
+	    <h1>Bad Request</h1>
+	    <p>Your request honestly kinda sucked.</p>
+	  </body>
+	</html>
+	`)
+}
+
+func respond500() []byte {
+	return []byte(`
+<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>
+	`)
+}
+
+func respond200() []byte {
+	return []byte(`
+<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>
+	`)
+}
+
 func main() {
-	httpServer, err := server.Serve(port, func(w io.Writer, req *request.Request) *server.HandlerError {
+	httpServer, err := server.Serve(port, func(w *response.Writer, req *request.Request) {
+
+		body := respond200()
+		statusLine := response.OK
 
 		if req.RequestLine.RequestTarget == "/yourproblem" {
-			e := server.HandlerError{
-				StatusCode: response.BadRequest,
-				Message:    []byte("Your problem is not my problem\n"),
-			}
-
-			return &e
+			body = respond400()
+			statusLine = response.BadRequest
 		}
 
 		if req.RequestLine.RequestTarget == "/myproblem" {
-			e := server.HandlerError{
-				StatusCode: response.InternalError,
-				Message:    []byte("Woopsie, my bad\n"),
-			}
-
-			return &e
+			body = respond500()
+			statusLine = response.InternalError
 		}
 
-		w.Write([]byte("All good, frfr\n"))
+		defaultHeaders := response.GetDefaultHeaders(len(body))
+		defaultHeaders.Replace("Content-Type", "text/html")
+		err := w.WriteStatusLine(statusLine)
+		if err != nil {
+			log.Fatalf("Error writing status line: %v", err)
+		}
 
-		return nil
+		err = w.WriteHeaders(defaultHeaders)
+		if err != nil {
+			log.Fatalf("Error writing headers: %v", err)
+		}
+
+		_, err = w.WriteBody([]byte(body))
+		if err != nil {
+			log.Fatalf("Error writing body: %v", err)
+		}
 	})
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
