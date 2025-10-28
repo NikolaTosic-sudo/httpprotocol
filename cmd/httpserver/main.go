d25b7f044d3a191a253c5d6f38d3ce5ee3123ec0
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-10-28 07:03:21 +0100

    Practice chunked encoding

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"httpprotocol/internal/request"
 	"httpprotocol/internal/response"
 	"httpprotocol/internal/server"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 )
 
@@ -60,17 +64,73 @@ func main() {
 		body := respond200()
 		statusLine := response.OK
 
+		defaultHeaders := response.GetDefaultHeaders(len(body))
+
 		if req.RequestLine.RequestTarget == "/yourproblem" {
 			body = respond400()
 			statusLine = response.BadRequest
+			strLen := strconv.Itoa(len(body))
+			defaultHeaders.Replace("Content-Length", strLen)
 		}
 
 		if req.RequestLine.RequestTarget == "/myproblem" {
 			body = respond500()
 			statusLine = response.InternalError
+			strLen := strconv.Itoa(len(body))
+			defaultHeaders.Replace("Content-Length", strLen)
 		}
 
-		defaultHeaders := response.GetDefaultHeaders(len(body))
+		if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+			target := req.RequestLine.RequestTarget
+			res, err := http.Get("https://httpbin.org/" + target[len("/httpbin/"):])
+
+			if err != nil {
+				body = respond500()
+				statusLine = response.InternalError
+				strLen := strconv.Itoa(len(body))
+				defaultHeaders.Replace("Content-Length", strLen)
+			} else {
+				w.WriteStatusLine(response.OK)
+
+				defaultHeaders.Delete("Content-Length")
+				defaultHeaders.Set("Transfer-encoding", "chunked")
+				defaultHeaders.Replace("Content-Type", "text/plain")
+
+				w.WriteHeaders(defaultHeaders)
+
+				for {
+					data := make([]byte, 32)
+					n, err := res.Body.Read(data)
+					if err != nil {
+						fmt.Println(err, "err")
+						break
+					}
+
+					_, err = w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
+
+					if err != nil {
+						fmt.Println(err, "hexa err")
+					}
+
+					_, err = w.WriteBody(data[:n])
+
+					if err != nil {
+						fmt.Println(err, "body err")
+					}
+
+					_, err = w.WriteBody([]byte("\r\n"))
+
+					if err != nil {
+						fmt.Println(err, "end err")
+					}
+				}
+
+				w.WriteBody([]byte("0\r\n\r\n"))
+
+				return
+			}
+		}
+
 		defaultHeaders.Replace("Content-Type", "text/html")
 		err := w.WriteStatusLine(statusLine)
 		if err != nil {
